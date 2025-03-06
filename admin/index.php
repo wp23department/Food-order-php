1f7b6c020a7478e6d21b26293c9cb0b186ffb8c3
wp23department <Wp23.department@gmail.com>
2025-03-06 18:38:26 +0530

    new file:   admin/index.php 	new file:   admin/manage-admin.php 	new file:   categories.html 	new file:   category-foods.html 	new file:   css/admin.css 	new file:   css/style.css 	new file:   food-search.html 	new file:   foods.html 	new file:   images/bg.jpg 	new file:   images/burger.jpg 	new file:   images/logo.png 	new file:   images/menu-burger.jpg 	new file:   images/menu-momo.jpg 	new file:   images/menu-pizza.jpg 	new file:   images/momo.jpg 	new file:   images/pizza.jpg 	new file:   index.html 	new file:   logo.png 	new file:   order.html

---
+++ admin/index.php
@@ -0,0 +1,63 @@
+<html>
+
+<head>
+    <title>food-Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu section start -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Admin</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">Food</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu section end -->
+
+    <!-- Main content section start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Dashbooard</h1>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                categories
+            </div>
+            <div class="col-4 text-center">
+                <h1>5</h1>
+                <br>
+                categories
+            </div>
+
+            <div class="clearfix">
+                 
+            </div>
+        </div>
+    </div>
+    <!-- Main content section end -->
+
+    <!-- Footer section start -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All right reserved, Some Restorent - At Devloped By - Amit Nanda </p> 
+        </div>
+    </div>
+    <!-- Footer section end -->
+</body>
+
+</html>
---
+++ admin/manage-admin.php
@@ -0,0 +1,45 @@
+<html>
+
+<head>
+    <title>food-Order Website - Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu section start -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Admin</a></li>
+                <li><a href="">Category</a></li>
+                <li><a href="">Food</a></li>
+                <li><a href="">order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu section end -->
+
+    <!-- Main content section start -->
+    <div class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+            
+
+            <div class="clearfix">
+                 
+            </div>
+        </div>
+    </div>
+    <!-- Main content section end -->
+
+    <!-- Footer section start -->
+    <div class="footer">
+        <div class="wrapper">
+            <p class="text-center">2020 All right reserved, Some Restorent - At Devloped By - Amit Nanda </p> 
+        </div>
+    </div>
+    <!-- Footer section end -->
+</body>
+
+</html>
